b3c4d579139d0db169b372c272b0934fba18ebfc
mrpedrobraga <mrhenribraga@gmail.com>
2023-10-28 13:49:36 -0300

    Sourcing data from profile.

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
 let beggining_of_time = new Date()
-let datasource = {
-	gender: "man",
-	gender_pl: "men" 
-}
-
 let population_data 
+let datasource
+const profile_input_elements = {}
+const hookups = {}
 
 /** Main function */
 async function main() {
 	population_data = await populationDataByCity(datasource)
+	datasource = await generateDataSource()
 
-	const elements = document.querySelectorAll(".hk")
-	const hookups = {}
+	// Retrieve the elements from the profile section.
+	const p_els = document.querySelectorAll(".profile-datum")
+	for (el of p_els) {
+		profile_input_elements[el.getAttribute('datum')] = el
+	}
 
-	for (el of elements) {
+	// Retrive the hookups from the rest of the page.
+	const hk_els = document.querySelectorAll(".hk")
+	for (el of hk_els) {
 		const dt = el.getAttribute('datum')
 		hookups[dt] ??= []
 		hookups[dt].push(el)
 	}
-
-
-	//fillData(hookups, datasource)
 }
 
+async function generateDataSource() {
+	let result = {}
+
+	result.gender = "man"
+	result.country = "United States"
+	result.city = "New York"
+	result.age = 14
+	result.alike_death_count = 30
+
+	return result
+}
+
+/** Organizes the population data by country and city */
 async function populationDataByCity() {
 	let population_data = await read_as_csv('./data/d_population.csv')
 
@@ -31,19 +45,42 @@ async function populationDataByCity() {
 	for (i in population_data.data) {
 		let el = population_data.data[i]
 
-		result[el[2]] ??= []
-		result[el[2]][el[1]] ??= []
-		result[el[2]][el[1]].push(el.slice(0))
-		console.log(el)
+		result[el[1]] ??= {}
+		result[el[1]][el[0]] = {
+			populatiopn: el[2]
+		}
 	}
 
 	return result
+}
+
+function update() {
+	for (const [k, el] of Object.entries(profile_input_elements)) {
+		datasource[k] = el.value
+	}
+
+	console.log(profile_input_elements)
+
+	datasource.gender_pl = {woman: 'women', man: 'men'}[datasource.gender]
+	
+	datasource.overall_death_count = 124
+	datasource.same_gender_death_count = 20
+	datasource.same_age_death_count = 4
+
+	console.log(datasource)
+
+	fillData(hookups, datasource)
 }
 
 /** Fills the HTML elements with the data from the source. */
 function fillData(hookups, source) {
 	for (const key of Object.keys(hookups)) {
 		let content = source[key]
+
+		if (!content) {
+			console.error(`Missing data for ${key}.`)
+			continue;
+		}
 
 		hookups[key].forEach((el) => {
 			if (typeof content == 'string') {
@@ -52,7 +89,8 @@ function fillData(hookups, source) {
 			}
 
 			if (typeof content == 'function') {
-				setInterval(content, 1, el)
+				el.innerHTML = content().toString
+				//setInterval(content, 1, el)
 				return
 			}
 
